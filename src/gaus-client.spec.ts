d98b7f4346ceac1acdf12e235ccb853a707cce64
Andreé Frank <andree.frank@sony.com>
2019-08-27 13:00:10 +0200

    Refactored so that session is not passed around
    Session is now internal to the GausClient class
    Wrote some basic tests around basic in param logic
    
    Issue: GAUS-1025

--- src/gaus-client.spec.ts
+++ src/gaus-client.spec.ts
@@ -5,4 +5,76 @@ describe('GausClient', (): void => {
     const client = new GausClient();
     expect(client).toBeTruthy();
   });
+
+  it('instantiates with session', (): void => {
+    const fakeSession = {
+      deviceGUID: 'fakeGausDeviceGUID',
+      productGUID: 'fakeGausProductGUID',
+      token: 'fakeGausToken',
+    };
+    const client = new GausClient(fakeSession);
+    expect(client).toBeTruthy();
+  });
+
+  it('register fails with falsy in parameters', (done): void => {
+    const client = new GausClient();
+    try {
+      client.register(null, '');
+      done.fail(new Error('Should throw error'));
+    } catch {
+      done();
+    }
+  });
+
+  it('register should return with correct in parameters ', (done): void => {
+    const client = new GausClient();
+    try {
+      const registerResponse = client.register({ accessKey: 'fakeAK', secretKey: 'fakeSK' }, 'fakeDID');
+      expect(registerResponse).toBeTruthy();
+      done();
+    } catch {
+      done.fail(new Error('Should throw error'));
+    }
+  });
+
+  it('authenticate fails with falsy in parameters', (done): void => {
+    const client = new GausClient();
+    try {
+      client.authenticate(null);
+      done.fail(new Error('Should throw error'));
+    } catch {
+      done();
+    }
+  });
+
+  it('authenticate should return with correct in parameters ', (done): void => {
+    const client = new GausClient();
+    try {
+      const session = client.authenticate({ accessKey: 'fakeAK', secretKey: 'fakeSK' });
+      expect(session).toBeTruthy();
+      done();
+    } catch {
+      done.fail(new Error('Should throw error'));
+    }
+  });
+
+  it('checkForUpdates fails with no valid session', (done): void => {
+    const client = new GausClient();
+    try {
+      client.checkForUpdates();
+      done.fail(new Error('Should throw error'));
+    } catch {
+      done();
+    }
+  });
+
+  it('report fails with no valid in parameters', (done): void => {
+    const client = new GausClient();
+    try {
+      client.report('', '', null);
+      done.fail(new Error('Should throw error'));
+    } catch {
+      done();
+    }
+  });
 });
--- src/gaus-client.ts
+++ src/gaus-client.ts
@@ -1,30 +1,50 @@
 export class GausClient {
-  constructor() {}
+  private _session: GausSession;
+
+  constructor(session?: GausSession) {
+    this._session = session || undefined;
+  }
 
   register(
     productAuthParameters: GausProductAuthParameters,
     deviceId: GausDeviceId
   ): { pollInterval: GausPollIntervalSeconds; deviceAuthParameters: GausDeviceAuthParameters } {
-    console.log(productAuthParameters); // to make the linter happy
-    console.log(deviceId); // to make the linter happy
-
-    return { pollInterval: 1, deviceAuthParameters: { accessKey: '', secretKey: '' } }; // Dummy code
+    if (productAuthParameters && productAuthParameters.accessKey && productAuthParameters.secretKey && deviceId) {
+      return { pollInterval: 1, deviceAuthParameters: { accessKey: '', secretKey: '' } }; // Dummy code
+    } else {
+      const error: GausError = { description: 'In parameters not defined' };
+      throw error;
+    }
   }
 
   authenticate(deviceAuthParameters: GausDeviceAuthParameters): GausSession {
-    console.log(deviceAuthParameters); // to make the linter happy
-
-    return { deviceGUID: '', productGUID: '', token: '' }; // Dummy code
+    if (deviceAuthParameters && deviceAuthParameters.accessKey && deviceAuthParameters.secretKey) {
+      return { deviceGUID: '', productGUID: '', token: '' }; // Dummy code
+    } else {
+      const error: GausError = { description: 'In parameters not defined' };
+      throw error;
+    }
   }
 
-  checkForUpdates(session: GausSession): GausUpdate[] {
-    console.log(session); // to make the linter happy
-
-    return []; // Dummy code
+  checkForUpdates(): GausUpdate[] {
+    if (this._session) {
+      return []; // Dummy code
+    } else {
+      const error: GausError = { description: 'No session initiated' };
+      throw error;
+    }
   }
 
-  report(session: GausSession, report: GausReport): void {
-    console.log(session); // to make the linter happy
-    console.log(report); // to make the linter happy
+  report(deviceGUID: GausDeviceGUID, productGUID: GausProductGUID, report: GausReport): void {
+    if (this._session) {
+      if (deviceGUID && productGUID && report && report.data && report.header && report.version) {
+      } else {
+        const error: GausError = { description: 'In parameters not defined' };
+        throw error;
+      }
+    } else {
+      const error: GausError = { description: 'No session initiated' };
+      throw error;
+    }
   }
 }
